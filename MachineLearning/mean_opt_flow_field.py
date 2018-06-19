7afbb018c4377885d7596502598e03a2309c0f47
Nicolas Castano <cococastano@yahoo.com>
2018-06-18 18:23:14 -0700

    removed redef method

--- MachineLearning/mean_opt_flow_field.py
+++ MachineLearning/mean_opt_flow_field.py
@@ -25,18 +25,6 @@ def draw_flow(img, flow, step=16):
         cv2.circle(vis, (x1, y1), 1, (0, 255, 0), -1)
     return vis
       
-def draw_flow(img, flow, step=16):
-    h, w = img.shape[:2]
-    y, x = np.mgrid[step/2:h:step, step/2:w:step].reshape(2,-1).astype(int)
-    fx, fy = flow[y,x].T
-    lines = np.vstack([x, y, x+fx, y+fy]).T.reshape(-1, 2, 2)
-    lines = np.int32(lines + 0.5)
-    vis = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-    cv2.polylines(vis, lines, 0, (0, 255, 0))
-    for (x1, y1), (_x2, _y2) in lines:
-        cv2.circle(vis, (x1, y1), 1, (0, 255, 0), -1)
-    return vis
-
 def draw_hsv(flow,frame, overlay=True):
     h, w = flow.shape[:2]
     fx, fy = flow[:,:,0], flow[:,:,1]
