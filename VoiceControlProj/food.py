3190d49cc3c2271608501978825e97deb710bac2
rajsr-p <42008987+rajsr-p@users.noreply.github.com>
2019-02-22 12:53:38 -0800

    food.py (version 5)

--- VoiceControlProj/food.py
+++ VoiceControlProj/food.py
@@ -1,23 +1,11 @@
-#!/usr/bin/env python3
-# Copyright 2017 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-"""A demo of the Google Assistant GRPC recognizer."""
-
 '''
-This class instantiates foodProcessor to get foods, allergens, and nutrients from the user's text.
-It then uses foodLog to log the information onto the Google Spreadsheet.
+Raj Palleti
+Last revision: 2/22/19
+
+This class instantiates foodProcessor to get foods, allergens, and nutrients 
+from the user's text, which is recognized using the Google Voice Kit.
+It then uses foodLog to log the information onto a Google Spreadsheet.
+
 '''
 
 import foodProcessor
